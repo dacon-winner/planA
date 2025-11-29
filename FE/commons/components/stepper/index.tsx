67d69f85d9901b9e6432a7209f9b21af45fc9f47
szeunkk <szeunkk@gmail.com>
2025-11-29 14:30:56 +0900

    refactor: Stepper 연결선 위치를 relative로 변경
    - position: absolute → relative (기본값) 으로 변경
    - left 속성 → marginLeft로 변경하여 flexbox 레이아웃 준수
    - connectorLineTop 스타일 제거 (불필요)
    - 커서룰 @02-wireframe.mdc 준수 (position-absolute 금지)

--- FE/commons/components/stepper/index.tsx
+++ FE/commons/components/stepper/index.tsx
@@ -138,9 +138,7 @@ export const StepItem: React.FC<StepItemProps> = ({
       </View>
 
       {/* Connector Line (마지막 스텝 제외, 폼이 닫혀있을 때만 표시) */}
-      {!isLast && !isOpen && (
-        <View style={[styles.connectorLine, styles.connectorLineTop]} />
-      )}
+      {!isLast && !isOpen && <View style={styles.connectorLine} />}
     </View>
   );
 };
